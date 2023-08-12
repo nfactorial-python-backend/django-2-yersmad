8615bac9ab9279ad3c60056097c183fc310bc70b
yersmad <yersmad@gmail.com>
2023-08-12 14:25:05 +0600

    solve: hw_2_django

--- news/urls.py
+++ news/urls.py
@@ -5,7 +5,7 @@ app_name = "news"
 
 urlpatterns = [
     path("", views.index, name="start_page"),
-    path("<int:new_id>/", views.detail, name="news_by_id"),
-    path("create_news/", views.create_news, name="create_news"),
-    
+    path("<int:news_id>/", views.detail, name="detail"),
+    path("<int:news_id>/add_comment", views.post_comment, name="post_comment"),
+    path("add_news", views.post_news, name="post_news"),
 ]   
--- news/views.py
+++ news/views.py
@@ -10,13 +10,40 @@ def index(request):
     context = {"news": news}
     return render(request, "news/index.html", context)
 
-def detail(request, new_id):
-    news = get_object_or_404(News, pk=new_id)
-    comments = get_list_or_404(Comment, news_id=new_id)
-    context = {"news": news, "comments": comments}
-    return render(request, "news/news_by_id.html", context)
+def detail(request, news_id):
+    news = get_object_or_404(News, pk=news_id)
+    try:
+        comments = Comment.objects.get(news_id=news_id)
+    except Comment.DoesNotExist:
+        comments = None
 
-def create_news(request):
-    title = request.POST.get("title")
-    content = request.POST.get("content")
-    return HttpResponseRedirect("news/index.html",)
+    if not comments:
+        context = {"news": news}
+        return render(request, "news/detail.html", context)
+    else:
+        comments = get_list_or_404(Comment, news_id=news_id)
+        context = {"news": news, "comments": comments}
+        return render(request, "news/detail.html", context)
+
+def post_comment(request, news_id):
+    comments = request.POST["comments"]
+
+    if comments != "":
+        news = get_object_or_404(News, pk=news_id)
+        db_comments = Comment(content=comments, news=news)
+        db_comments.save()
+
+        return HttpResponseRedirect(reverse("news:post_comment", args=news_id,))
+    
+    return HttpResponseRedirect(reverse("news:post_comment", args=news_id,))
+
+def post_news(request):
+    if request.method == "POST":
+        title = request.POST["title"]
+        content = request.POST["content"]
+        news = News(title=title, content=content)
+        news.save()
+
+        return HttpResponseRedirect(reverse("news:post_news", args=news.id))
+
+    return render(request, "news/post_news.html")
