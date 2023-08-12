bf84a38961d0f618375c8848575680940e5ca692
yersmad <yersmad@gmail.com>
2023-08-12 15:00:15 +0600

    solve: 3,4 task of hw_3

--- news/admin.py
+++ news/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import News, Comment
 # Register your models here.
 
-admin.site.register(News)
-admin.site.register(Comment)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 5
+
+
+class AdminNews(admin.ModelAdmin):
+    field = ["title", "content", "created_at", "has_comments"]
+    inlines = [CommentInline]
+
+
+admin.site.register(News, AdminNews)
--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ class News(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
+    has_comments = models.BooleanField(default=False)
 
 class Comment(models.Model):
     content = models.CharField(max_length=100)
